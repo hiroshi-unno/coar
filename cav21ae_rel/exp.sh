f8ae56d34cccb68642d0326dfa32a677bdce6c88
Hiroshi Unno <uhiro@cs.tsukuba.ac.jp>
2021-04-30 12:38:39 +0900

    fix a script for CAV21 experiments on hyperproperties verification

--- cav21ae_rel/exp.sh
+++ cav21ae_rel/exp.sh
@@ -16,7 +16,8 @@ echo -e "\n\nTS_GNI_hFF:"; time timeout 1200 _build/default/main.exe -c config/s
 echo -e "\n\nTS_GNI_hTT_hint:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore_hyperprop2.json -p pcsp benchmarks/cav2021rel/TS_GNI_hTT_hint.clp
 echo -e "\n\nHalfSquareNI:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore_hyperprop2.json -p pcsp benchmarks/cav2021rel/HalfSquareNI.clp
 echo -e "\n\nArrayInsert:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore_hyperprop4.json -p pcsp benchmarks/cav2021rel/ArrayInsert.clp
-echo -e "\n\nSquareSum:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore_hyperprop3.json -p pcsp benchmarks/cav2021rel/SquareSum.clp
+#echo -e "\n\nSquareSum:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore_hyperprop3.json -p pcsp benchmarks/cav2021rel/SquareSum.clp
+echo -e "\n\nSquareSum_hint:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore_hyperprop3.json -p pcsp benchmarks/cav2021rel/SquareSum_hint.clp
 echo -e "\n\nSimpleTS_GNI1:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore.json -p pcsp benchmarks/cav2021rel/SimpleTS_GNI1.clp
 echo -e "\n\nSimpleTS_GNI2:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore.json -p pcsp benchmarks/cav2021rel/SimpleTS_GNI2.clp
 echo -e "\n\nInfBranchTS_GNI:"; time timeout 1200 _build/default/main.exe -c config/solver/pcsat_tb_ucore.json -p pcsp benchmarks/cav2021rel/InfBranchTS_GNI.clp
